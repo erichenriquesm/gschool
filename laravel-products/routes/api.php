681e8ece13962d090839fb1ce28d63d28ccb5611
Eric Motta <eric@greenn.com.br>
2024-03-18 20:14:08 -0300

    ok

--- laravel-products/routes/api.php
+++ laravel-products/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,11 +20,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 
-Route::post('/teste', function (Request $request){
-    // $name = $request->input('name');
-    // return $name;
-    return $request->all();
-});
+Route::post('/product', [ProductController::class, 'store']);
 
 // $request->all() = retorna todos os dados da request
 // $request->input() = retorna todos os inputs da request
